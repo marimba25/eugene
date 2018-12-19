91624e6625d8dbd3a643efbd69a601abc7a97117
marina <inwaiting2010@gmail.com>
2018-12-19 23:24:17 +0300

    logo

--- eugene/child/models.py
+++ eugene/child/models.py
@@ -6,16 +6,17 @@ class Child(models.Model):
     birthday = models.DateField(verbose_name='дата рождения')
 
     def __str__(self):
-        return self.name
+        return self.name or "No name"
 
 
 class Photo(models.Model):
-    title = models.CharField(verbose_name='название фотографии', max_length=100, default='Мимими')
+    title = models.CharField(verbose_name='название фотографии', max_length=100, blank=True)
     child = models.ForeignKey('Child', on_delete=models.CASCADE)
     file = models.ImageField(upload_to='child/photo')
-    description = models.TextField(verbose_name='описание фотографии')
+    description = models.TextField(verbose_name='описание фотографии', blank=True)
     date = models.DateTimeField(verbose_name='дата', auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.title or 'No title'
+
 
